f8922bd40d836fefc22c37ad40942e8123e8dd69
Слава <slavaatworks@gmail.com>
2021-01-26 18:52:10 +0200

    react game: exp

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,6 @@
 
 import React from "react"
-import Board from "./components/TikTakToe/Board"
+import Board from "./components/BoardElements/Board"
 import styles from "./Game.module.css"
 class Game extends React.Component {
   constructor(props) {
