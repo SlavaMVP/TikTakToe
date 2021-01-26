48e632a4624e87e413b393c5131b30ea79f39805
Слава <slavaatworks@gmail.com>
2021-01-26 15:44:01 +0200

    react game: add o player

--- src/components/TikTakToe/Board.js
+++ src/components/TikTakToe/Board.js
@@ -1,19 +1,23 @@
 import React from "react"
-import Square from "./Square"
+import Square from "./square"
 
 class Board extends React.Component {
     constructor(props) {
-      super(props);
-      this.state = {
-        squares: Array(9).fill(null),
-      };
-    }
+        super(props);
+        this.state = {
+          squares: Array(9).fill(null),
+          xIsNext: true,
+        };
+      }
   
-    handleClick(i) {
-      const squares = this.state.squares.slice();
-      squares[i] = 'X';
-      this.setState({squares: squares});
-    }
+      handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+          squares: squares,
+          xIsNext: !this.state.xIsNext,
+        });
+      }
   
     renderSquare(i) {
       return (
@@ -25,7 +29,7 @@ class Board extends React.Component {
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
--- src/components/TikTakToe/Square.js
+++ src/components/TikTakToe/Square.js
@@ -1,16 +1,9 @@
-import React from "react"
-
-class Square extends React.Component {
-    render() {
-      return (
-        <button
-          className="square"
-          onClick={() => this.props.onClick()}
-        >
-          {this.props.value}
-        </button>
-      );
-    }
+function square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
   }
 
-  export default Square;
+  export default square;
